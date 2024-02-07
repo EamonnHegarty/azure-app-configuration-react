e82e9eb09a5cd73ca0e823c9653ed050e84cfce6
Eamonn Hegarty <eamonnhegartyy@hotmail.co.uk>
2024-02-07 10:47:09 +0000

    changes to deploy

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
       <h1>Vite + React</h1>
-      <p className="read-the-docs">Making changes to deploy</p>
+      <p>Making changes to deploy</p>
       <h1>This should not be visible when feature flag is false</h1>
     </>
   );
