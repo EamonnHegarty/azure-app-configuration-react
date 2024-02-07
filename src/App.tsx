13febcb15ea657fad09a8a27f26e428161637d58
Eamonn Hegarty <eamonnhegartyy@hotmail.co.uk>
2024-02-07 10:26:39 +0000

    pushing to make a deployment

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,7 @@ function App() {
   return (
     <>
       <h1>Vite + React</h1>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Making changes to deploy</p>
       <h1>This should not be visible when feature flag is false</h1>
     </>
   );
