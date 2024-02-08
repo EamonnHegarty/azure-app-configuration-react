df7870294c7d86097d4aa70e99dfb9d8c4687581
Eamonn Hegarty <eamonnhegartyy@hotmail.co.uk>
2024-02-08 16:58:50 +0000

    added request and feature flag implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,62 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import axios from "axios";
+
+const isLocal = import.meta.env.VITE_APP_ENV === "local";
+
+type FeatureFlag = {
+  enabledHeader: boolean;
+  enabledFooter: boolean;
+};
+
+type FeatureFlagResponse = {
+  id: string;
+  flags: FeatureFlag;
+};
+
+const api = isLocal
+  ? "http://localhost:8080"
+  : "https://azure-feature-flags-api.azurewebsites.net";
 
 function App() {
+  const [featureFlags, setFeatureFlags] = useState<FeatureFlagResponse[]>([]);
+  const [enabledHeader, setEnabledHeader] = useState(false);
+  const [enabledFooter, setEnabledFooter] = useState(false);
+
+  const fetchFeatureFlags = () => {
+    axios
+      .get(`${api}/feature-flags`)
+      .then((response) => {
+        setFeatureFlags(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching feature flags:", error);
+      });
+  };
+
+  useEffect(() => {
+    fetchFeatureFlags();
+  }, []);
+
+  useEffect(() => {
+    if (featureFlags.length > 0) {
+      const { flags } = featureFlags[0];
+      setEnabledHeader(flags.enabledHeader);
+      setEnabledFooter(flags.enabledFooter);
+    }
+  }, [featureFlags]);
+
   return (
     <>
-      <h1>Vite + React</h1>
-      <p>Making changes to deploy</p>
-      <h1>This should not be visible when feature flag is false</h1>
+      {enabledHeader && <h1>The Header</h1>}
+      <button
+        onClick={() =>
+          console.log([featureFlags, enabledHeader, enabledFooter])
+        }
+      >
+        Log Data
+      </button>
+      {enabledFooter && <h1 style={{ marginTop: 10 }}>The Footer</h1>}
     </>
   );
 }
