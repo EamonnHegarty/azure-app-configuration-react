d155159ec32e8eda614d845fdfcc2908ff48af1e
Eamonn Hegarty <eamonnhegartyy@hotmail.co.uk>
2024-02-12 13:17:56 +0000

    deleted app config in azure created new one due to exposed secret

--- src/azureConfigFetcher.ts
+++ src/azureConfigFetcher.ts
@@ -1,7 +1,7 @@
 import { AppConfigurationClient } from "@azure/app-configuration";
 
 const client = new AppConfigurationClient(
-  "Endpoint=https://azure-feature-flags-app-config.azconfig.io;Id=Mb8z;Secret=NXVJy2tHMXKKx6NvS8zb4riFzbErcYpEh7H4B/chcUM="
+  `${import.meta.env.VITE_APP_CONFIG_CONNECTION_STRING}`
 );
 
 //TODO: Add Connection String copied from Access Keys Step
